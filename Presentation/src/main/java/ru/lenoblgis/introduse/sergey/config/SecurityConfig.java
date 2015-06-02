17b834a2473181e5af020676da09d3e0a2ab05c3
Sergey <vilgodskiy@gmail.com>
2015-06-02 18:33:50 +0300

    Начало работы со spring security

--- Presentation/src/main/java/ru/lenoblgis/introduse/sergey/config/SecurityConfig.java
+++ Presentation/src/main/java/ru/lenoblgis/introduse/sergey/config/SecurityConfig.java
@@ -1,11 +1,59 @@
 package ru.lenoblgis.introduse.sergey.config;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.jdbc.core.JdbcTemplate;
+import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
 
-public class SecurityConfig  /*extends WebScurityConfigurerAdapter*/ {
+import com.microsoft.sqlserver.jdbc.SQLServerDataSource;
 
-	/*@Autowired
-	public void configureGlobal(A){
-		
-	}*/
+import ru.lenoblgis.introduse.sergey.domen.user.UserRole;
+
+public class SecurityConfig extends WebSecurityConfigurerAdapter{
+
+	SQLServerDataSource ds = null;
+	
+	public SecurityConfig() {
+		super();
+		beforeConstruct();
+	}
+
+	@Autowired
+	public void configureGlobal(AuthenticationManagerBuilder auth) throws Exception{
+		auth
+			.jdbcAuthentication()
+				.dataSource(ds);
+		//TODO:
+	}
+
+	@Override
+	protected void configure(HttpSecurity http) throws Exception {
+		http
+			.authorizeRequests()
+				.antMatchers("/registration", "/login", "/hello").permitAll()
+				.antMatchers("admin/**").hasRole(UserRole.ADMIN.getName())
+				.antMatchers("organization/**", "passport/**").authenticated()
+				.anyRequest().authenticated()
+					.and()
+			.logout()
+				.logoutUrl("/logout")
+				.logoutSuccessUrl("/hello")
+				.and()
+			.formLogin()
+				.loginPage("/authorization")
+				.permitAll();
+			
+	}
+	
+	public void beforeConstruct(){
+		ds = new SQLServerDataSource();
+		ds.setPortNumber(1433);
+		ds.setHostNameInCertificate("localhost");
+		ds.setDatabaseName("passport_agricultural");
+		ds.setUser("adminAgricultural");
+		ds.setPassword("admin123");
+	}
+	
+	
 }
